a02400aaf37e5e0b6da0a04d0a6d960477106215
Elizaveta <krylovaliza@yandex.ru>
2022-11-12 00:20:09 +0300

    Create personskill.js

---
+++ HRProject/models/personskill.js
@@ -0,0 +1,88 @@
+const { Pool } = require("pg");
+
+const credentials = {
+    user: "root",
+    host: "postgres",
+    password: "root",
+};
+const pool = new Pool(credentials);
+
+const addPersonSkill = async(data) => {
+    try {
+        const results = findRecordByPersonSkill(data).then(async foundRecord => {
+            if (foundRecord == undefined) {
+                var sql = `INSERT INTO personskill(person_personid, skill_skillid) VALUES('${data.personid}', '${data.skillid}')`;
+                await pool.query(sql);
+                return 'true';
+            }
+            return 'already exists';
+        })
+        return results;
+    } catch (error) {
+        console.error(error.stack);
+        return 'false';
+    }
+}
+
+const findRecordBySkill = async(skillid) => {
+    var sql = `
+            SELECT * FROM personskill WHERE skill_skillid = ${skillid}
+            `;
+    const results = await pool
+        .query(sql)
+        .then((data) => {
+            return data.rows;
+        })
+    return results;
+}
+
+const findRecordByPerson = async(personid) => {
+    var sql = `
+            SELECT * FROM personskill WHERE person_personid = ${personid}
+            `;
+    const results = await pool
+        .query(sql)
+        .then((data) => {
+            return data.rows;
+        })
+    return results;
+}
+
+const findRecordByPersonSkill = async(data) => {
+    var sql = `
+            SELECT * FROM personskill WHERE (person_personid = ${data.personid} AND skill_skillid = ${data.skillid})
+            `;
+    const results = await pool
+        .query(sql)
+        .then((data) => {
+            return data.rows[0];
+        })
+    return results;
+}
+
+const findByPerson = (req, res) => {
+    const data = req.body
+    findRecordByPerson(data).then((results) => {
+        res.status(200).json(JSON.stringify(results))
+    }).catch((error) => {
+        console.log(error)
+        res.status(500).send()
+    })
+}
+
+const findBySkill = (req, res) => {
+    const data = req.body
+    findRecordBySkill(data).then((results) => {
+        res.status(200).json(JSON.stringify(results))
+    }).catch((error) => {
+        console.log(error)
+        res.status(500).send()
+    })
+}
+
+module.exports = {
+    addPersonSkill,
+    findByPerson,
+    findBySkill,
+    findRecordByPersonSkill
+}

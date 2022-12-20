a28ed9a2d219531d56109e649710a99f0217455c
fatalliska <62687203+fatalliska@users.noreply.github.com>
2022-12-20 21:14:33 +0300

    Add files via upload

--- HRProject/public/script.js
+++ HRProject/public/script.js
@@ -7,6 +7,7 @@ fetch("/employees")
 
             card.querySelector("h4").innerText = employee.login;
             card.id = employee.personid;
+            card.employee = employee.login;
     //         fetch("/skills").then((response) => response.json()).then((data) => {
     //             data.forEach((skill) => {
     //                 if(skill.personid==employee.personid)  {
@@ -17,3 +18,23 @@ fetch("/employees")
             document.body.appendChild(card);
         });
     });
+
+    function find(id, employee)
+    {
+        window.location.href='skills.html'
+        fetch("/personskills/"+id)
+        .then((response) => response.json())
+        .then((data) => {
+            var skills = [];
+            data.forEach((skill) => {
+                skills.push(skill);
+            });
+        const cardTemplate = document.querySelector("template");
+        const card = cardTemplate.content.cloneNode(true);
+        console.log(cardTemplate);
+        card.querySelector("h2").innerText = employee.login;
+        card.querySelector("br").innerText = skills.join(', ');
+        document.body = card;
+    });
+    }
+    
